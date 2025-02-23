318d68f6f8418e50499269f56ee31b6ffe470f1c
Nishant <nishantraj839@gmail.com>
2025-02-24 04:50:39 +0530

    minor change

--- src/main/java/com/embarkx/jobapp/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/embarkx/jobapp/review/impl/ReviewServiceImpl.java
@@ -85,6 +85,7 @@ public class ReviewServiceImpl implements ReviewService {
             Review review = reviewRepository.findById(reviewId).orElse(null);
             Company company = review.getCompany();
             company.getReviews().remove(review);
+            review.setCompany(null);
             companyService.updateCompany(companyId, company);
             reviewRepository.deleteById(reviewId);
 
