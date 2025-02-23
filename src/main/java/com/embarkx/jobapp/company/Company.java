c880420aec425ec85bcb8c895b456f65bf1cb86a
Nishant <nishantraj839@gmail.com>
2025-02-24 01:38:23 +0530

    REST APIs, controller for Companies

--- src/main/java/com/embarkx/jobapp/company/Company.java
+++ src/main/java/com/embarkx/jobapp/company/Company.java
@@ -9,7 +9,6 @@ import java.util.List;
 public class Company
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
--- src/main/java/com/embarkx/jobapp/company/CompanyService.java
+++ src/main/java/com/embarkx/jobapp/company/CompanyService.java
@@ -5,6 +5,8 @@ import java.util.List;
 public interface CompanyService {
 
     List<Company> getAllCompanies();
-
-
+    boolean updateCompany(Long id,Company company);
+    void createCompany(Company company);
+    boolean deleteCompany(Long id);
+    Company getCompanyById(Long id);
 }
